1e2177c0f558b94ac4bf85fad4b9ce37c25abb11
osawldeff <tialem94@gmail.com>
2022-04-18 18:36:54 +0900

    feat: 채팅앱 index, chatroom 추가

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
+import json
+
 
 # Create your views here.
+
+
+def index(request):
+    context = {
+    }
+    return render(request, 'index.html', context)
+
+
+def chatroom(request, room_name):
+    context = {
+        'room_name': mark_safe(json.dumps(room_name))
+    }
+    return render(request, 'chatroom.html', context)
