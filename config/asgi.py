64aa8b6c3803016bf3a51d71fada92b059bd8cb5
osawldeff <tialem94@gmail.com>
2022-04-18 18:53:46 +0900

    feat: asgi 실행파일 dotenv및 django setup설정

--- config/asgi.py
+++ config/asgi.py
@@ -8,8 +8,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+dotenv_path = Path(__file__).resolve().parent.parent
+load_dotenv(dotenv_path)
+
+django.setup()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
