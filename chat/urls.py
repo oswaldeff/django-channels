fd6e4d48597e36960be9e3b5f4ad315c233814de
osawldeff <tialem94@gmail.com>
2022-04-18 18:38:40 +0900

    feat: index, chatroom에 대한 urlpattern추가

--- chat/urls.py
+++ chat/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import index, chatroom
 
 
 urlpatterns = [
+    path('', index, name='index'),
+    path('<str:room_name>/', chatroom, name='chatroom'),
 ]
